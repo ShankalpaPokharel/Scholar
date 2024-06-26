59a9b5b89c288a77eab7090d46b270dd5316eae0
ShankalpaPokharel <pokharelshankalpa58@gmail.com>
2024-06-26 17:30:39 +0545

    Testimonials finished

--- src/components/TestimonialBanner.jsx
+++ src/components/TestimonialBanner.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import member1 from "../assets/member-01.jpg";
 
-export default function () {
+export default function ({ banner }) {
   return (
-    <div className="rounded-3xl bg-primary p-20 text-white">
-      <p className="mb-10 text-lg font-light italic leading-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente qui tenetur praesentium nostrum maxime ipsa dignissimos enim ipsam et, laudantium rerum.</p>
+    <div className="rounded-3xl bg-primary p-10 text-white lg:p-20">
+      <p className="mb-10 text-lg font-light italic leading-10">{banner.testimonial}</p>
       <div className="flex items-center">
-        <img className="max-w-[100px] mr-6 rounded-full" src={member1} alt="" />
+        <img className="mr-6 max-w-[100px] rounded-full" src={banner.image} alt="" />
         <div>
-          <span className="text-sm">Full Stack Master</span>
-          <h4 className="mt-2 text-xl font-semibold">skdfsd fsdlkfj</h4>
+          <span className="text-sm">{banner.expertField}</span>
+          <h4 className="mt-2 text-xl font-semibold">{banner.name}</h4>
         </div>
       </div>
     </div>
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import member1 from "../assets/member-01.jpg";
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -8,6 +9,30 @@ import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import TestimonialBanner from "./TestimonialBanner";
 
 export default function Testimonials() {
+  const testimoni = [
+    {
+      _id: 1,
+      name: "Thomas Jefferson",
+      expertField: "UI Expert",
+      testimonial: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravid.”",
+      image: member1,
+    },
+    {
+      _id: 2,
+      name: "Stella Blair",
+      expertField: "Digital Animator",
+      testimonial: "“Scholar is free website template provided by TemplateMo for educational related websites. This CSS layout is based on Bootstrap v5.3.0 framework.”",
+      image: member1,
+    },
+    {
+      _id: 3,
+      name: "Claude David",
+      expertField: "Full Stack Master",
+      testimonial: "“Please tell your friends or collegues about TemplateMo website. Anyone can access the website to download free templates. Thank you for visiting.”",
+      image: member1,
+    },
+  ];
+
   let settings = {
     // dots: true,
     adaptiveHeight: true,
@@ -30,20 +55,20 @@ export default function Testimonials() {
     }
   };
   return (
-    <div className="lg:testimonials relative mt-36 py-20">
-      <div className="container relative flex items-center ">
+    <div className="lg:testimonials relative lg:mt-36 lg:py-20">
+      <div className="container relative items-center lg:flex">
         {/* banner part  */}
         <div className="relative lg:mr-16 lg:w-[58%]">
           <Slider {...settings} className="" ref={sliderRef}>
-            {/* {bannerData.map((el) => (
-                <Banner key={el.id} banner={el} />
-              ))} */}
-            <TestimonialBanner />
-            <TestimonialBanner />
+            {testimoni?.map((el) => (
+              <TestimonialBanner key={el._id} banner={el} />
+            ))}
+            {/* <TestimonialBanner/>
+            <TestimonialBanner/> */}
           </Slider>
 
           {/* sliding button */}
-          <div className="absolute hidden -right-5 top-1/2 -translate-y-7 flex-col justify-center gap-4 text-white lg:flex">
+          <div className="absolute -right-5 top-1/2 hidden -translate-y-7 flex-col justify-center gap-4 text-white lg:flex">
             <div className={`w-fit rounded-full bg-white p-3`} onClick={previous}>
               <FaChevronLeft className="text-primary" />
             </div>
@@ -53,7 +78,7 @@ export default function Testimonials() {
           </div>
         </div>
         {/* body part  */}
-        <div className="lg:w-[41%]">
+        <div className="mt-10 lg:mt-0 lg:w-[41%]">
           <span className="text-sm font-semibold uppercase text-primary">testimonials</span>
           <h2 className="my-5 text-4xl font-semibold capitalize leading-10">What they say about us?</h2>
           <p className="mt-12 text-sm leading-7 text-textlight">You can search free CSS templates on Google using different keywords such as templatemo portfolio, templatemo gallery, templatemo blue color, etc.</p>
