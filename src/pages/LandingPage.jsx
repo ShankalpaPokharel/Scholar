910d26d93f866f3aacc6ccae6c6273e10d7767bd
ShankalpaPokharel <pokharelshankalpa58@gmail.com>
2024-06-26 13:45:20 +0545

    document format

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { Aboutus, Facts, HeroPart, LatestCourses, Navbar, OurFeatures } from '../components'
+import React from "react";
+import { Aboutus, Facts, HeroPart, LatestCourses, Navbar, OurFeatures } from "../components";
 
 export default function LandingPage() {
   return (
-   <>
-    <div className='bg-bannerback bg-cover  bg-right bg-no-repeat pb-32'>
-      <Navbar/>
-      <HeroPart/>
-    </div>
-    <OurFeatures/>
-    <Aboutus/>
-    <LatestCourses/>
-    <Facts/>
-   </>
-  )
+    <>
+      <div className="bg-bannerback bg-cover bg-right bg-no-repeat pb-32">
+        <Navbar />
+        <HeroPart />
+      </div>
+      <OurFeatures />
+      <Aboutus />
+      <LatestCourses />
+      <Facts />
+    </>
+  );
 }
